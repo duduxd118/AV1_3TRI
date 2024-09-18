e020c89b7dcedf9f4d39bbfeb12fb80fd77cb2d2
duduxd118 <167109877+duduxd118@users.noreply.github.com>
2024-09-18 10:47:36 -0300

    ,

--- perguntas.js
+++ perguntas.js
@@ -13,7 +13,7 @@ criaCartao(
 criaCartao(
     'Programação',
     'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Uma função é bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
