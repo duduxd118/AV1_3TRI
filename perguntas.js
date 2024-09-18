cf73908b253e8b6e39862ea591a7dcdbb29ed6e7
duduxd118 <167109877+duduxd118@users.noreply.github.com>
2024-09-18 10:52:11 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'conhecimento gerais',
+    'De onde é a invenção do chuveiro elétrico?',
+    'A invenção do chuveriro eletrico foi no brasil'
 )
 
 criaCartao(
